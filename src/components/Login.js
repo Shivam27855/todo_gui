ac9ef161aeeb153c48a050e67ba53da321daad4a
Shivam27855 <shivamsingh27855@gmail.com>
2022-12-15 12:08:41 +0530

    My first commit for gui in git repo

---
+++ src/components/Login.js
@@ -0,0 +1,83 @@
+import '../App.css';
+import { useState, useEffect } from "react";
+import ToDoItem from './TodoItem';
+function Login() {
+    //const baseURL = "http://localhost:5000";
+    const baseURL ="https://calm-lime-antelope-vest.cyclic.app";
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  const [checkLoginStatus,setCheckLoginStatus]=useState(false);
+  const [todoItems,setTodoItems]=useState([]);
+  const [userId,setUserId]=useState();
+  const [emptyToDoList,setEmptyToDoList]=useState(false);
+  let handleLogOut =(e)=>{
+    e.preventDefault();
+    setCheckLoginStatus(false);
+  }
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      fetch(`${baseURL}/getUser`, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: username,
+          password:password
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          if (json.length != 0) {
+            console.log(json);
+            setCheckLoginStatus(true);
+
+            if(json.error=="No Todo Item")
+            {
+                setEmptyToDoList(true);
+            }
+            else{
+                setTodoItems(json);
+                setEmptyToDoList(false);
+            }
+            
+            fetch(`${baseURL}/getUserId`, {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                  username: username,
+                  password:password
+                }),
+              })
+                .then((res) => res.json())
+                .then((json) => {
+                  if (json.length != 0) {
+                    //alert(json)
+                    setUserId(json);
+            
+                  }
+                })
+          }
+        })
+    } catch (err) {
+      alert("Login Fail")
+      console.log(err);
+    }
+  };
+
+  let handleUserNameChange=(e)=>{
+    setUsername(e.target.value);
+  }
+
+  let handlePasswordChange=(e)=>{
+    setPassword(e.target.value);
+  }
+
+
+
+  return (checkLoginStatus==false?<form onSubmit={handleSubmit}>
+    <input type="text" value={username} placeholder="USERNAME" onChange={handleUserNameChange}/>
+    <input type="password" value={password} placeholder="PASSWORD" onChange={handlePasswordChange}/>
+    <button type="submit">LOGIN</button>
+  </form>:<div><button type="button" onClick={handleLogOut}>LOG OUT</button>{<ToDoItem emptyToDoList={emptyToDoList} item={todoItems} userId={userId} />}</div>);
+}
+export default Login;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,205 @@
+import logo from '../logo.svg';
+import '../App.css';
+import { useState, useEffect } from "react";
+import Login from './Login';
+
+
+function ToDoItem(props) {
+  const [todoItems,setTodoItems]=useState(props.item);
+  const [emptyToDoList,setEmptyToDoList]=useState(props.emptyToDoList);
+  const [newItem,setNewItem]=useState();
+  let handleEdit=(todoId)=>
+  {
+  //const baseURL = "http://localhost:5000";
+  const baseURL ="https://calm-lime-antelope-vest.cyclic.app";
+  try {
+    fetch(`${baseURL}/editTodoItem/${todoId}`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        todoItem: "username"
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.length != 0) {
+          console.log(json);
+
+          // setCheckLoginStatus(true);
+          // setTodoItems(json);
+          // setProfileUserName(json[0].userName);
+
+          // fetch(
+          //   `${baseURL}/getAccount/${profileUserId}`)
+          //   .then((res) => res.json())
+          //   .then((json) => {
+          //     setAccount(json);
+          //   })
+        }
+      })
+  } catch (err) {
+    alert("Login Fail")
+    console.log(err);
+  }
+}
+
+useEffect(() => {}, [emptyToDoList]); // <- add the count variable here
+
+
+
+let handleDelete=(todoId)=>
+{
+//const baseURL = "http://localhost:5000";
+const baseURL ="https://calm-lime-antelope-vest.cyclic.app";
+//alert(props.userId);
+
+try {
+  fetch(`${baseURL}/deleteTodoItem/${todoId}`, {
+    method: "GET",
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+
+      //alert(currentUserId);
+
+      try {
+        fetch(`${baseURL}/getToDoItems`, {
+          method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          userId:parseInt(props.userId)
+        }),
+        })
+          .then((res) => res.json())
+          .then((json) => {
+
+            if(json.error=="No Todo Item")
+            {
+              console.log(json);
+                setEmptyToDoList(true);
+                console.log(emptyToDoList);
+                //alert(emptyToDoList);
+                
+            }
+            else{
+                setTodoItems(json);
+                console.log(emptyToDoList);
+                //setEmptyToDoList(false);
+            }
+
+            //setTodoItems(json);
+            console.log(todoItems)
+          })
+      } catch (err) {
+        alert("Delete Fail")
+        console.log(err);
+      }
+
+
+
+
+
+
+    })
+} catch (err) {
+  alert("Delete Fail")
+  console.log(err);
+}
+}
+
+
+let handleAddText=(e)=>{
+  setNewItem(e.target.value);
+}
+
+let handleAdd=()=>
+{
+//const baseURL = "http://localhost:5000";
+const baseURL ="https://calm-lime-antelope-vest.cyclic.app";
+//alert(props.userId);
+
+try {
+  fetch(`${baseURL}/addTodoItem`, {
+    method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          todoItem: newItem
+        }),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+
+      //alert(currentUserId);
+
+      try {
+        fetch(`${baseURL}/getToDoItems`, {
+          method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          userId:parseInt(props.userId)
+        }),
+        })
+          .then((res) => res.json())
+          .then((json) => {
+
+            if(json.error=="No Todo Item")
+            {
+              console.log(json);
+                setEmptyToDoList(true);
+                console.log(emptyToDoList);
+                //alert(emptyToDoList);
+                
+            }
+            else{
+                setTodoItems(json);
+                console.log(emptyToDoList);
+                setEmptyToDoList(false);
+            }
+
+            //setTodoItems(json);
+            console.log(todoItems)
+          })
+      } catch (err) {
+        alert("Delete Fail")
+        console.log(err);
+      }
+
+
+
+
+
+
+    })
+} catch (err) {
+  alert("Delete Fail")
+  console.log(err);
+}
+}
+
+
+
+  return (
+    <div>
+      <h1>adfas {props.userId}</h1>
+      <input type="text" value={newItem} placeholder="Enter Item" onChange={handleAddText}/><button onClick={handleAdd}>Create Item</button>
+      {emptyToDoList==false?<ul>
+    {todoItems.map(
+      (todoItem) => <li key={todoItem.todoId}>
+                          {todoItem.todoItem}
+                          <button onClick={()=>handleEdit(todoItem.todoId)}>Edit</button>
+                          <button onClick={()=>handleDelete(todoItem.todoId)}>Delete</button>
+                    </li>
+                  )};
+  </ul>:<h1>No Todo Item to show</h1>}
+
+    </div>
+    
+  );
+  
+  
+  
+}
+export default ToDoItem;
